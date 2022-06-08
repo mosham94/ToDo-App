63a3daba9482d04539cde8d9e4a9eb4ddf8b7e3f
mosham94 <107154331+mosham94@users.noreply.github.com>
2022-06-09 03:44:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+
+import './App.css';
+
+
+function App() {
+  const [toDos,setTodos] = useState([])
+  const [toDo,setTodo] = useState('')
+  let weekday = ['SunDay', 'MonDay', 'TuesDay', 'WednesDay', 'ThursDay', 'FriDay', 'SaturDay'][new Date().getDay()]
+
+  return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Hola, it's {weekday}  ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=> setTodo(e.target.value)} type="text" placeholder=" 🖊️  Add item..." />
+        <i onClick={()=>setTodos([...toDos,{id:Date.now() ,text: toDo, status:false}])} className="fas fa-plus"></i>
+      </div>
+  
+      <div className="todos">
+    {    toDos.map((obj)=>{
+
+ return ( <div className="todo">
+<div className="left">
+  <input onChange={(e)=>{
+    console.log(e.target.checked)
+    console.log(obj)
+    setTodos(toDos.filter(obj2=>{
+    if(obj2.id===obj.id){
+      obj2.status = e.target.checked
+    }  
+    return obj2
+    }))
+  } 
+  } value={obj.status} type="checkbox" name="" id="" />
+  <p>{obj.text}</p>
+</div>
+<div className="right">
+  <i onClick={()=>setTodos([])} className="fas fa-times"></i>
+</div>
+</div>)
+
+    })
+
+}
+<div>
+{
+
+  toDos.map((obj)=>{
+    if(obj.status){
+      return(<h1>{ obj.id}</h1>)
+    }
+    else{
+      return(<h1>HELLO</h1>)
+    }
+    return null
+  })
+  
+}
+</div>
+      </div>
+</div>
+   
+  );
+}
+
+export default App;
